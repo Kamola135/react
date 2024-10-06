ff1701081f03156e8a50b62a7d1f5940ebed2d42
Macbook Air <macbookair@Keyn-mac.local>
2024-10-06 22:09:22 +0500

    Обновление проекта React

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,12 @@ import Welcome from "./pages/Welcome";
 import "./styles/main.css";
 import StepTwo from "./pages/StepTwo";
 import StepOne from "./pages/StepOne";
+import StepThree from "./pages/StepThree";
 
 const App = () => {
   return (
     <div className="App">
-      <Welcome />
-      <StepOne />
-      <StepTwo />
+      <StepThree />
     </div>
   );
 };
--- src/pages/StepThree.jsx
+++ src/pages/StepThree.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import AppHeader from "../components/AppHeader";
+import AppButton from "../components/AppButton";
 
 const StepThree = () => {
+  const [selectedVariant, setSelectedVariant] = useState(null);
+  const handleVariantChange = (event) => {
+    setSelectedVariant(event.target.value);
+  };
+
+  const isButtonDisabled = selectedVariant === null;
+
   return (
     <div className="container">
       <div className="wrapper">
         <div className="emoji-quiz">
+          <AppHeader
+            headerType="h2"
+            headerText="3. Занимательный вопрос"
+          />
           <div className="indicator">
             <div className="indicator__text">
               <span className="indicator__description">
@@ -19,42 +32,30 @@ const StepThree = () => {
               <div className="indicator__unit indicator__unit-4"></div>
             </div>
           </div>
-          <div className="question">
-            <h2>3. Занимательный вопрос</h2>
-            <ul className="emoji-variants">
-              <li className="variant-wrapper">
-                <input required type="radio" name="variant" id="variant-1" />
-                <label htmlFor="variant-1">
-                  <img src="./img/laugh.png" alt="laugh" />
-                  <p>Ваш ответ 1</p>
+          <ul className="emoji-variants">
+            {[1, 2, 3, 4].map((variant) => (
+              <li className="variant-wrapper" key={variant}>
+                <input
+                  required
+                  type="radio"
+                  name="variant"
+                  id={`variant-${variant}`}
+                  value={variant}
+                  onChange={handleVariantChange}
+                />
+                <label htmlFor={`variant-${variant}`}>
+                  <img src={`./img/variant-${variant}.png`} alt={`variant-${variant}`} />
+                  <p>Ваш ответ {variant}</p>
                 </label>
               </li>
-              <li className="variant-wrapper">
-                <input required type="radio" name="variant" id="variant-2" />
-                <label htmlFor="variant-2">
-                  <img src="./img/hearts.png" alt="hearts" />
-                  <p>Ваш ответ 2</p>
-                </label>
-              </li>
-              <li className="variant-wrapper">
-                <input required type="radio" name="variant" id="variant-3" />
-                <label htmlFor="variant-3">
-                  <img src="./img/smirk.png" alt="smirk" />
-                  <p>Ваш ответ 3</p>
-                </label>
-              </li>
-              <li className="variant-wrapper">
-                <input required type="radio" name="variant" id="variant-4" />
-                <label htmlFor="variant-4">
-                  <img src="./img/fright.png" alt="fright" />
-                  <p>Ваш ответ 4</p>
-                </label>
-              </li>
-            </ul>
-            <button type="button" disabled id="next-btn">
-              Далее
-            </button>
-          </div>
+            ))}
+          </ul>
+          <AppButton
+            buttonText="Далее"
+            isDisabled={isButtonDisabled}
+            buttonClick={() => {
+            }}
+          />
         </div>
       </div>
     </div>
